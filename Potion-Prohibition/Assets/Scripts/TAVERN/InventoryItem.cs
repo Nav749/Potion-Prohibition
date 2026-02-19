d584d064e516d7352710456a60b48ceb1b6c400c
Nav749 <navingunaratnam77@gmail.com>
2026-02-18 20:20:07 -0500

    got the item to check what is gonna be given

--- Potion-Prohibition/Assets/Scripts/TAVERN/InventoryItem.cs
+++ Potion-Prohibition/Assets/Scripts/TAVERN/InventoryItem.cs
@@ -7,7 +7,16 @@ using UnityEngine.EventSystems;
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
+    [SerializeField] Potion potion;
     [HideInInspector] public Transform parentAfterDrag;
+    [HideInInspector] public string potionName;
+
+    public void Start()
+    {
+        image = GetComponent<Image>();
+        image.sprite = potion.getImage();
+        potionName = potion.getName();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
--- Potion-Prohibition/Assets/Scripts/TAVERN/InventorySlot.cs
+++ Potion-Prohibition/Assets/Scripts/TAVERN/InventorySlot.cs
@@ -5,6 +5,27 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+
+    [SerializeField] bool isChecker = false;
+    private bool internalbool = true;
+
+    public void Update()
+    {
+        if (isChecker)
+        {
+            if (this.transform.childCount != 0 && internalbool) 
+            {
+                InventoryItem potionToCheck = this.transform.GetChild(0).gameObject.transform.GetComponent<InventoryItem>();
+                Debug.Log(potionToCheck.potionName);
+                internalbool = false;
+            }
+            else if(this.transform.childCount == 0)
+            {
+                internalbool = true;
+            }
+        }
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         if(transform.childCount == 0)
