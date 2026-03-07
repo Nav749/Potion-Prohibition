e8ed2ea96fb4073667fe0910b0731b7b7fd9185c
Nav749 <navingunaratnam77@gmail.com>
2026-03-07 11:22:30 -0500

    Added a prefab to spawn random objects
    To be used to add variety to room generation

---
+++ Potion-Prohibition/Assets/Scripts/DUNGEON/SpawnProps.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProps : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> spawnableProps;
+
+    void Start()
+    {
+        int randint = Random.Range(0, spawnableProps.Count);
+        Instantiate(spawnableProps[randint], this.transform.position, Quaternion.identity);
+    }
+
+}
