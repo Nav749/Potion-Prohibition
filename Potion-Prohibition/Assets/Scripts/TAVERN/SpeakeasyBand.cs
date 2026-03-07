9903f5377205357ff89657dfd011180b4f3c6e56
Nav749 <navingunaratnam77@gmail.com>
2026-03-07 14:12:06 -0500

    fixed audio issue, working on changing song

--- Potion-Prohibition/Assets/Scripts/TAVERN/SpeakeasyBand.cs
+++ Potion-Prohibition/Assets/Scripts/TAVERN/SpeakeasyBand.cs
@@ -1,21 +1,61 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpeakeasyBand : MonoBehaviour
 {
 
     [SerializeField] List<AudioClip> songs;
-    private AudioSource source;
+    public AudioSource[] source = new AudioSource[2];
+    private AudioClip newsong;
+    private bool activeSource;
+    bool isFading = false;
+    IEnumerator musicTransition;
 
-    void Start()
+    void Awake()
     {
-        source = GetComponent<AudioSource>();
-        source.clip = songs[0];
+        newsong = songs[Random.Range(0, songs.Count - 1)];
+        PlayCrossfade(newsong);
     }
 
-    void Update()
+    void PlayCrossfade(AudioClip clip)
     {
+        int nextSource = !activeSource ? 0 : 1;
+        int currentSource = activeSource ? 0 : 1;
+
+        if(clip == source[currentSource].clip)
+        {
+            return;
+        }
+
+        if(musicTransition != null)
+        {
+            StopCoroutine(musicTransition);
+        }
+
+        source[nextSource].clip = clip;
+        source[nextSource].Play();
+
+        musicTransition = Crossfade(20);
+        StartCoroutine(musicTransition);
         
+    }
+
+    IEnumerator Crossfade(int time)
+    {
+        for (int i = 0; i < time + 1; i++)
+        {
+            source[0].volume = activeSource ? (time - i) * (1f / time) : (0 + i) * (1f / time);
+            source[1].volume = !activeSource ? (time - i) * (1f / time) : (0 + i) * (1f / time);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        source[activeSource ? 0 : 1].Stop();
+
+        activeSource = !activeSource;
+        musicTransition = null;
     }
 }
