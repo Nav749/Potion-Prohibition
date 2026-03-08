91cce5bae6087bff11933cdeac1ef26cd7ae4c9c
Nav749 <navingunaratnam77@gmail.com>
2026-03-08 19:22:54 -0400

    dialogue works!

--- Potion-Prohibition/Assets/Scripts/TAVERN/TriggerForEavesdrop.cs
+++ Potion-Prohibition/Assets/Scripts/TAVERN/TriggerForEavesdrop.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using TMPro;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class TriggerForEavesdrop : MonoBehaviour
@@ -7,19 +10,86 @@ public class TriggerForEavesdrop : MonoBehaviour
     [SerializeField] CustomerDialogue dialogue;
     private string[] lines;
 
+    public TextMeshProUGUI textComponent;
+    public float textSpeed;
+    public GameObject TextBubble;
+
+    private int index;
+
     private void Start()
     {
         lines = dialogue.currentCustomers.GetDialogue();
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (playerListening)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (textComponent.text == lines[index])
+                {
+                    NextLine();
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    textComponent.text = lines[index];
+                }
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        textComponent.text = string.Empty;
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            TextBubble.SetActive(false);
+            index = 0;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            for(int i = 0; i < lines.Length; i++)
-            {
-                Debug.Log(lines[i]);
-            }
+            playerListening = true;
+            TextBubble.SetActive(true);
+            StartDialogue();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerListening = false;
+            TextBubble.SetActive(false);
+            StopAllCoroutines();
         }
     }
 }
