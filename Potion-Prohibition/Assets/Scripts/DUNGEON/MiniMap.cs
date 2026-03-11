570bd94417fa7abfd04d3c18214214878364e703
Nav749 <navingunaratnam77@gmail.com>
2026-03-11 17:05:51 -0400

    minimap base made

---
+++ Potion-Prohibition/Assets/Scripts/DUNGEON/MiniMap.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameManager.Instance.PlayerGO.transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
