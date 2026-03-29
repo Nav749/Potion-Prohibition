e8bb6f1699337b92bc82a020483ab3beef1d113d
TS2006 <teakernes@gmail.com>
2026-03-29 16:23:59 -0400

    the clugest of bug fixes in the trutorial

--- Potion-Prohibition/Assets/Scripts/Tutorial/Tutorial Book.cs
+++ Potion-Prohibition/Assets/Scripts/Tutorial/Tutorial Book.cs
@@ -12,8 +12,10 @@ public class TutorialBook : MonoBehaviour
 
     private int background;
     [SerializeField] private GameObject[] backGrounds;
-    [SerializeField] private int backgroundOnePos;
-    //[SerializeField] private int backgroundTwoPos;
+    [SerializeField] private int sectionOnePos;
+    [SerializeField] private int sectionTwoPos;
+    [SerializeField] private int sectionThreePos;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -44,6 +46,7 @@ public class TutorialBook : MonoBehaviour
         toggleBackground(open);
         genralUI.SetActive(open);
         toggleCursor();
+        GameObject.Find("Player").GetComponentInChildren<playerLook>().locked = open;
 
     }
 
@@ -65,12 +68,9 @@ public class TutorialBook : MonoBehaviour
     {
         if (page < pages.Length - 1)
         {
-            if (page + 1 == backgroundOnePos)
-            {
-                switchBackground(background++);
-            }
             pages[page].SetActive(false);
             page++;
+            switchBackground(page);
             pages[page].SetActive(true);
         }
     }
@@ -79,27 +79,34 @@ public class TutorialBook : MonoBehaviour
     {
         if (page > 0)
         {
-            if (page - 1 == backgroundOnePos)
-            {
-                switchBackground(background--);
-            }
             pages[page].SetActive(false);
             page--;
+            switchBackground(page);
             pages[page].SetActive(true);
-
         }
     }
 
     public void goToPage(int index)
     {
-
+        if (index > -1 && index < pages.Length)
+        {
+            pages[page].SetActive(false);
+            page = index;
+            switchBackground(page);
+            pages[page].SetActive(true);
+        }
     }
+
+    public void goToSectionOne() { goToPage(sectionOnePos); }
+    public void goToSectionTwo() { goToPage(sectionTwoPos); }
+    public void goToSectionThree() { goToPage(sectionThreePos); }
+
 
     private void toggleBackground(bool input)
     {
         if (input)
         {
-            switchBackground(background);
+            backGrounds[background].SetActive(true);
 
         }
         else
@@ -110,16 +117,47 @@ public class TutorialBook : MonoBehaviour
 
     private void switchBackground(int index)
     {
-        for (int i = 0; i < backGrounds.Length; i++)
+        if (index == sectionOnePos)
         {
-            if (i == index)
-            {
-                backGrounds[i].SetActive(true);
-            }
-            else
-            {
-                backGrounds[i].SetActive(false);
-            }
+            backGrounds[background].SetActive(false);
+            background = 0;
+            backGrounds[background].SetActive(true);
+
         }
+        else if (index > sectionOnePos && index < sectionTwoPos)
+        {
+            backGrounds[background].SetActive(false);
+            background = 1;
+            backGrounds[background].SetActive(true);
+
+
+        }
+        else if (index == sectionTwoPos)
+        {
+            backGrounds[background].SetActive(false);
+            background = 2;
+            backGrounds[background].SetActive(true);
+        }
+        else if (index > sectionTwoPos && index < sectionThreePos)
+        {
+            backGrounds[background].SetActive(false);
+            background = 3;
+            backGrounds[background].SetActive(true);
+        }
+        else if (index == sectionThreePos)
+        {
+            backGrounds[background].SetActive(false);
+            background = 4;
+            backGrounds[background].SetActive(true);
+        }
+        else if (index > sectionThreePos)
+        {
+            backGrounds[background].SetActive(false);
+            background = 5;
+            backGrounds[background].SetActive(true);
+        }
+
+
+
     }
 }
