cadc03b13924b01bbd44d9f0d17ba6987a20f1d5
Nav749 <navingunaratnam77@gmail.com>
2026-03-28 20:03:41 -0400

    fixed mouse sens

--- Potion-Prohibition/Assets/Scripts/PLAYER/playerLook.cs
+++ Potion-Prohibition/Assets/Scripts/PLAYER/playerLook.cs
@@ -17,8 +17,8 @@ public class playerLook : MonoBehaviour
     void Update()
     {
         
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
        
         xRotation -= mouseY;
