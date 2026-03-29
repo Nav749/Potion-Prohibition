f666f45db2de774b443ce595836b34b93b29b01e
Nav749 <navingunaratnam77@gmail.com>
2026-03-29 15:12:28 -0400

    there is now a skip button for the comic

--- Potion-Prohibition/Assets/Scripts/ComicMove.cs
+++ Potion-Prohibition/Assets/Scripts/ComicMove.cs
@@ -5,6 +5,7 @@ public class ComicMove : MonoBehaviour
 {
     public float limit;
     public GameObject Buttons;
+    public GameObject SkipButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +21,7 @@ public class ComicMove : MonoBehaviour
         else if(this.transform.position.y > limit)
         {
             Buttons.SetActive(true);
+            SkipButton.SetActive(false);
         }
     }
 
@@ -31,5 +33,10 @@ public class ComicMove : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Persistent");
+    }
+
+    public void Skip()
+    {
+        this.transform.position = new Vector3(0, limit + 0.01f, 0);
     }
 }
