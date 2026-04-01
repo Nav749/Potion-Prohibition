05b4eacd4f18f08613285128b72ada45e91f9e35
Nav749 <navingunaratnam77@gmail.com>
2026-04-01 00:52:28 -0400

    fixed bug in tutorial with pause on enter

---
+++ Potion-Prohibition/Assets/Scripts/TimeScaleReset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TimeScaleReset : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
