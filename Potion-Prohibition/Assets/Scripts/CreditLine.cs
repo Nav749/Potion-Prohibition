04245e753ce40867e33216d42b6de08dca2ba4fb
TS2006 <teakernes@gmail.com>
2026-04-01 09:13:33 -0400

    fun stuff for piper

--- Potion-Prohibition/Assets/Scripts/CreditLine.cs
+++ Potion-Prohibition/Assets/Scripts/CreditLine.cs
@@ -8,18 +8,21 @@ public class CreditLine : MonoBehaviour
     [SerializeField] private Image PFPImage;
     [SerializeField] private string Credit;
     [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private string desc;
+    [SerializeField] private TextMeshProUGUI descTMP;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PFPImage.sprite = PFP;
         textMeshPro.text = Credit;
+        descTMP.text = desc;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
